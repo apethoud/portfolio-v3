a6d9acdda415f9f3b5c214c0b84daee5640392e1
apethoud <andrewpethoud@gmail.com>
2017-10-31 12:04:31 -0700

    feature(routing): added app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing/app-routing.module'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './app-routing/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
