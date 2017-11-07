f373d95556bc09f0878902188c084a612a8b38e1
apethoud <andrewpethoud@gmail.com>
2017-11-06 17:50:12 -0800

    feature(routing): added functionality to dynamically create list of case studies

--- src/app/app-routing/home/home.component.ts
+++ src/app/app-routing/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Study } from '../../study';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  studies = [
+    new Study('Foresight Budgets', 'Description of Foresight Budgets'),
+    new Study('OrdrSlip Kiosk', 'Description of OrdrSlip Kiosk'),
+    new Study('App Skin', 'Description of App Skin'),
+    new Study('Learn Node Project', 'Description of Learn Node Project'),
+    new Study('Shopping List App', 'Description of Shopping List App'),
+    new Study('Robot Fight Club Logo', 'Description of Robot Fight Club Logo')
+  ];
 
   constructor() { }
 
---
+++ src/app/study.ts
@@ -0,0 +1,5 @@
+export class Study {
+  constructor(
+    public name: string,
+    public description: string) {}
+}
