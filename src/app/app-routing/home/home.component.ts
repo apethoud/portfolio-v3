ad5478d010250d3408ed55e4482241596d348bb4
apethoud <andrewpethoud@gmail.com>
2017-11-06 22:05:39 -0800

    feature(home): added functionality to dynamically populate list of software used

--- src/app/app-routing/home/home.component.ts
+++ src/app/app-routing/home/home.component.ts
@@ -9,12 +9,12 @@ import { Study } from '../../study';
 export class HomeComponent implements OnInit {
 
   studies = [
-    new Study('Foresight Budgets', 'Description of Foresight Budgets'),
-    new Study('OrdrSlip Kiosk', 'Description of OrdrSlip Kiosk'),
-    new Study('App Skin', 'Description of App Skin'),
-    new Study('Learn Node Project', 'Description of Learn Node Project'),
-    new Study('Shopping List App', 'Description of Shopping List App'),
-    new Study('Robot Fight Club Logo', 'Description of Robot Fight Club Logo')
+    new Study('Foresight Budgets', 'Description of Foresight Budgets', ['HTML', 'CSS', 'AngularJS', 'Illustrator']),
+    new Study('OrdrSlip Kiosk', 'Description of OrdrSlip Kiosk', ['HTML', 'CSS', 'Angular 2', 'Illustrator']),
+    new Study('App Skin', 'Description of App Skin', ['HTML', 'CSS', 'Angular 2']),
+    new Study('Learn Node Project', 'Description of Learn Node Project', ['HTML', 'CSS', 'Angular 2', 'MongoDB']),
+    new Study('Shopping List App', 'Description of Shopping List App', ['HTML', 'CSS', 'Angular 2']),
+    new Study('Robot Fight Club Logo', 'Description of Robot Fight Club Logo', ['Illustrator'])
   ];
 
   constructor() { }
--- src/app/study.ts
+++ src/app/study.ts
@@ -1,5 +1,6 @@
 export class Study {
   constructor(
     public name: string,
-    public description: string) {}
+    public description: string,
+    public software: any = null) {}
 }
