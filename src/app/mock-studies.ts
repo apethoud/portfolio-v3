ae1ce6c175c9a5ce8c9e42b39cc0d42143a2e34e
apethoud <andrewpethoud@gmail.com>
2018-01-20 16:07:53 -0800

    feat(foresight icons): added featured item copy

--- src/app/mock-studies.ts
+++ src/app/mock-studies.ts
@@ -380,59 +380,53 @@ export const STUDIES: Study[] = [
     featuredColIndex: [
       {
         column: 1,
-        featureOrder: [1, 2, 3]
+        featureOrder: [1, 2]
       },
       {
         column: 2,
-        featureOrder: [4, 5]
+        featureOrder: [3, 4]
       },
       {
         column: 3,
-        featureOrder: [6, 7]
+        featureOrder: [5, 6]
       }
     ],
     featuredItems: [
       {
         id: 1,
         icon: "http://via.placeholder.com/80x80",
-        title: "Concept",
-        content: "A personal finance app designed to be forward-thinking and accurate."
+        title: "Project Goal",
+        content: "Create a set of icons to convey our unique app features in a fun and engaging way."
       },
       {
         id: 2,
         icon: "http://via.placeholder.com/80x80",
-        title: "The Story Behind It",
-        content: "I was personally fed up with using poorly-designed finance management software for my everyday finances, so I recruited a team of developers to help me design a better one for a local coding competition."
+        title: "My Role",
+        content: "Responsible for entire design process from initial concepting through final product."
       },
       {
         id: 3,
         icon: "http://via.placeholder.com/80x80",
-        title: "My Role",
-        content: ['Concept Creator', 'Development Team Leader', 'Lead UX/UI Designer', 'Icon Designer']
+        title: "How They Were Used",
+        content: ["Print marketing materials", "Website graphics", "Trade show booth display"]
       },
       {
         id: 4,
         icon: "http://via.placeholder.com/80x80",
-        title: "Project Goal",
-        content: "Gain experience designing an app from scratch."
+        title: "Greatest Challenge",
+        content: "Early on, I had issues with readability as I scaled the icons. Details I created would get lost as I scaled down, despite using a consistent line width in my forms."
       },
       {
         id: 5,
         icon: "http://via.placeholder.com/80x80",
-        title: "Project Outcome",
-        content: "Created a functional and beautifully-designed app."
+        title: "A Lesson Learned",
+        content: "Prior to this project, I knew that using a consistent line width and strategy throughout the icon set would serve to unify the icons into a cohesive collection. While working on this project, I learned that using a consistent width and strategy throughout my use of negative space is equally as important. Maintaining both guarantees that the forms are scalable and readable."
       },
       {
         id: 6,
         icon: "http://via.placeholder.com/80x80",
         title: "Most Proud Of",
-        content: "The progress my team and I were able to make in such a short time."
-      },
-      {
-        id: 7,
-        icon: "http://via.placeholder.com/80x80",
-        title: "Greatest Challenge",
-        content: "Interfacing with a third-party API for importing bank transactions securely."
+        content: "By strategically using dashed lines to convey certain details, I was able to further unify the icons into a cohesive set."
       },
     ],
     designStrategy: {
